fe1b37b9398a660707d8f1f5477932ceca164a6a
Ahmed Qarmout <ahmedqarmout2@gmail.com>
2024-06-05 18:21:48 -0400

    Started work on parsing raymath functions

--- raylib/raylib_unrecognized.h
+++ raylib/raylib_unrecognized.h
@@ -1,5 +1,10 @@
 #ifndef RAYLIB_H
+RAYLIB_H
 #include <stdarg.h>     // Required for: va_list - Only used by TraceLogCallback
+RAYLIB_VERSION_MAJOR 5
+RAYLIB_VERSION_MINOR 0
+RAYLIB_VERSION_PATCH 0
+RAYLIB_VERSION  "5.0"
 #if defined(_WIN32)
     #if defined(BUILD_LIBTYPE_SHARED)
         #if defined(__TINYC__)
@@ -42,12 +47,26 @@
 #if !defined(_MSC_VER) && (defined(__cplusplus) && __cplusplus < 201103L)
     #error "C++11 or later is required. Add -std=c++11"
 #endif
+RL_COLOR_TYPE
+RL_RECTANGLE_TYPE
+RL_VECTOR2_TYPE
+RL_VECTOR3_TYPE
+RL_VECTOR4_TYPE
+RL_QUATERNION_TYPE
+RL_MATRIX_TYPE
 #if (defined(__STDC__) && __STDC_VERSION__ >= 199901L) || (defined(_MSC_VER) && _MSC_VER >= 1800)
     #include <stdbool.h>
 #elif !defined(__cplusplus) && !defined(bool)
     typedef enum bool { false = 0, true = !false } bool;
     #define RL_BOOL_TYPE
 #endif
+MOUSE_LEFT_BUTTON   MOUSE_BUTTON_LEFT
+MOUSE_RIGHT_BUTTON  MOUSE_BUTTON_RIGHT
+MOUSE_MIDDLE_BUTTON MOUSE_BUTTON_MIDDLE
+MATERIAL_MAP_DIFFUSE      MATERIAL_MAP_ALBEDO
+MATERIAL_MAP_SPECULAR     MATERIAL_MAP_METALNESS
+SHADER_LOC_MAP_DIFFUSE      SHADER_LOC_MAP_ALBEDO
+SHADER_LOC_MAP_SPECULAR     SHADER_LOC_MAP_METALNESS
 #if defined(__cplusplus)
 extern "C" {            // Prevents name mangling of functions
 #endif
