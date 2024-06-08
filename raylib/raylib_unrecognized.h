8d6b09cc626c56fff552c7f8cfdf6aeb4a7a25c7
Ahmed Qarmout <ahmedqarmout2@gmail.com>
2024-06-07 22:14:49 -0400

    Added support to parse more function parts

--- raylib/raylib_unrecognized.h
+++ raylib/raylib_unrecognized.h
@@ -1,72 +1,77 @@
 #ifndef RAYLIB_H
-RAYLIB_H
+#define RAYLIB_H
+
 #include <stdarg.h>     // Required for: va_list - Only used by TraceLogCallback
-RAYLIB_VERSION_MAJOR 5
-RAYLIB_VERSION_MINOR 0
-RAYLIB_VERSION_PATCH 0
-RAYLIB_VERSION  "5.0"
+
+#define RAYLIB_VERSION_MAJOR 5
+#define RAYLIB_VERSION_MINOR 0
+#define RAYLIB_VERSION_PATCH 0
+#define RAYLIB_VERSION  "5.0"
+
+// Function specifiers in case library is build/used as a shared library (Windows)
+// NOTE: Microsoft specifiers to tell compiler that symbols are imported/exported from a .dll
 #if defined(_WIN32)
-    #if defined(BUILD_LIBTYPE_SHARED)
-        #if defined(__TINYC__)
-            #define __declspec(x) __attribute__((x))
-        #endif
+#if defined(BUILD_LIBTYPE_SHARED)
+#if defined(__TINYC__)
+#define __declspec(x) __attribute__((x))
+#endif
         #define RLAPI __declspec(dllexport)     // We are building the library as a Win32 shared library (.dll)
     #elif defined(USE_LIBTYPE_SHARED)
         #define RLAPI __declspec(dllimport)     // We are using the library as a Win32 shared library (.dll)
     #endif
 #endif
 #ifndef RLAPI
-    #define RLAPI       // Functions defined as 'extern' by default (implicit specifiers)
+#define RLAPI       // Functions defined as 'extern' by default (implicit specifiers)
 #endif
 #ifndef PI
-    #define PI 3.14159265358979323846f
+#define PI 3.14159265358979323846f
 #endif
 #ifndef DEG2RAD
-    #define DEG2RAD (PI/180.0f)
+#define DEG2RAD (PI/180.0f)
 #endif
 #ifndef RAD2DEG
-    #define RAD2DEG (180.0f/PI)
+#define RAD2DEG (180.0f/PI)
 #endif
 #ifndef RL_MALLOC
-    #define RL_MALLOC(sz)       malloc(sz)
+#define RL_MALLOC(sz)       malloc(sz)
 #endif
 #ifndef RL_CALLOC
-    #define RL_CALLOC(n,sz)     calloc(n,sz)
+#define RL_CALLOC(n,sz)     calloc(n,sz)
 #endif
 #ifndef RL_REALLOC
-    #define RL_REALLOC(ptr,sz)  realloc(ptr,sz)
+#define RL_REALLOC(ptr,sz)  realloc(ptr,sz)
 #endif
 #ifndef RL_FREE
-    #define RL_FREE(ptr)        free(ptr)
+#define RL_FREE(ptr)        free(ptr)
 #endif
 #if defined(__cplusplus)
-    #define CLITERAL(type)      type
+#define CLITERAL(type)      type
 #else
-    #define CLITERAL(type)      (type)
+#define CLITERAL(type)      (type)
 #endif
 #if !defined(_MSC_VER) && (defined(__cplusplus) && __cplusplus < 201103L)
-    #error "C++11 or later is required. Add -std=c++11"
+#error "C++11 or later is required. Add -std=c++11"
 #endif
-RL_COLOR_TYPE
-RL_RECTANGLE_TYPE
-RL_VECTOR2_TYPE
-RL_VECTOR3_TYPE
-RL_VECTOR4_TYPE
-RL_QUATERNION_TYPE
-RL_MATRIX_TYPE
+#define RL_COLOR_TYPE
+#define RL_RECTANGLE_TYPE
+#define RL_VECTOR2_TYPE
+#define RL_VECTOR3_TYPE
+#define RL_VECTOR4_TYPE
+#define RL_QUATERNION_TYPE
+#define RL_MATRIX_TYPE
 #if (defined(__STDC__) && __STDC_VERSION__ >= 199901L) || (defined(_MSC_VER) && _MSC_VER >= 1800)
-    #include <stdbool.h>
+#include <stdbool.h>
 #elif !defined(__cplusplus) && !defined(bool)
-    typedef enum bool { false = 0, true = !false } bool;
-    #define RL_BOOL_TYPE
+typedef enum bool { false = 0, true = !false } bool;
+#define RL_BOOL_TYPE
 #endif
-MOUSE_LEFT_BUTTON   MOUSE_BUTTON_LEFT
-MOUSE_RIGHT_BUTTON  MOUSE_BUTTON_RIGHT
-MOUSE_MIDDLE_BUTTON MOUSE_BUTTON_MIDDLE
-MATERIAL_MAP_DIFFUSE      MATERIAL_MAP_ALBEDO
-MATERIAL_MAP_SPECULAR     MATERIAL_MAP_METALNESS
-SHADER_LOC_MAP_DIFFUSE      SHADER_LOC_MAP_ALBEDO
-SHADER_LOC_MAP_SPECULAR     SHADER_LOC_MAP_METALNESS
+#define MOUSE_LEFT_BUTTON   MOUSE_BUTTON_LEFT
+#define MOUSE_RIGHT_BUTTON  MOUSE_BUTTON_RIGHT
+#define MOUSE_MIDDLE_BUTTON MOUSE_BUTTON_MIDDLE
+#define MATERIAL_MAP_DIFFUSE      MATERIAL_MAP_ALBEDO
+#define MATERIAL_MAP_SPECULAR     MATERIAL_MAP_METALNESS
+#define SHADER_LOC_MAP_DIFFUSE      SHADER_LOC_MAP_ALBEDO
+#define SHADER_LOC_MAP_SPECULAR     SHADER_LOC_MAP_METALNESS
 #if defined(__cplusplus)
 extern "C" {            // Prevents name mangling of functions
 #endif
--- raylib/raymath_unrecognized.h
+++ raylib/raymath_unrecognized.h
@@ -1,199 +1,100 @@
 #ifndef RAYMATH_H
-RAYMATH_H
+#define RAYMATH_H
+
 #if defined(RAYMATH_IMPLEMENTATION) && defined(RAYMATH_STATIC_INLINE)
-    #error "Specifying both RAYMATH_IMPLEMENTATION and RAYMATH_STATIC_INLINE is contradictory"
+#error "Specifying both RAYMATH_IMPLEMENTATION and RAYMATH_STATIC_INLINE is contradictory"
 #endif
 #if defined(RAYMATH_IMPLEMENTATION)
-    #if defined(_WIN32) && defined(BUILD_LIBTYPE_SHARED)
-        #define RMAPI __declspec(dllexport) extern inline // We are building raylib as a Win32 shared library (.dll).
-    #elif defined(_WIN32) && defined(USE_LIBTYPE_SHARED)
-        #define RMAPI __declspec(dllimport)         // We are using raylib as a Win32 shared library (.dll)
-    #else
-        #define RMAPI extern inline // Provide external definition
-    #endif
+#if defined(_WIN32) && defined(BUILD_LIBTYPE_SHARED)
+#define RMAPI __declspec(dllexport) extern inline // We are building raylib as a Win32 shared library (.dll).
+#elif defined(_WIN32) && defined(USE_LIBTYPE_SHARED)
+#define RMAPI __declspec(dllimport)         // We are using raylib as a Win32 shared library (.dll)
+#else
+#define RMAPI extern inline // Provide external definition
+#endif
 #elif defined(RAYMATH_STATIC_INLINE)
     #define RMAPI static inline // Functions may be inlined, no external out-of-line definition
 #else
-    #if defined(__TINYC__)
-        #define RMAPI static inline // plain inline not supported by tinycc (See issue #435)
-    #else
-        #define RMAPI inline        // Functions may be inlined or external definition used
-    #endif
+#if defined(__TINYC__)
+#define RMAPI static inline // plain inline not supported by tinycc (See issue #435)
+#else
+#define RMAPI inline        // Functions may be inlined or external definition used
+#endif
 #endif
 #ifndef PI
-    #define PI 3.14159265358979323846f
+#define PI 3.14159265358979323846f
 #endif
 #ifndef EPSILON
-    #define EPSILON 0.000001f
+#define EPSILON 0.000001f
 #endif
 #ifndef DEG2RAD
-    #define DEG2RAD (PI/180.0f)
+#define DEG2RAD (PI/180.0f)
 #endif
 #ifndef RAD2DEG
-    #define RAD2DEG (180.0f/PI)
+#define RAD2DEG (180.0f/PI)
 #endif
 #ifndef MatrixToFloat
-    #define MatrixToFloat(mat) (MatrixToFloatV(mat).v)
+#define MatrixToFloat(mat) (MatrixToFloatV(mat).v)
 #endif
 #ifndef Vector3ToFloat
-    #define Vector3ToFloat(vec) (Vector3ToFloatV(vec).v)
+#define Vector3ToFloat(vec) (Vector3ToFloatV(vec).v)
 #endif
 #if !defined(RL_VECTOR2_TYPE)
-RL_VECTOR2_TYPE
+// Vector2 type
+typedef struct Vector2 {
+float x;
+float y;
+} Vector2;
+#define RL_VECTOR2_TYPE
 #endif
 #if !defined(RL_VECTOR3_TYPE)
-RL_VECTOR3_TYPE
+// Vector3 type
+typedef struct Vector3 {
+float x;
+float y;
+float z;
+} Vector3;
+#define RL_VECTOR3_TYPE
 #endif
 #if !defined(RL_VECTOR4_TYPE)
-RL_VECTOR4_TYPE
+// Vector4 type
+typedef struct Vector4 {
+float x;
+float y;
+float z;
+float w;
+} Vector4;
+#define RL_VECTOR4_TYPE
 #endif
 #if !defined(RL_QUATERNION_TYPE)
-RL_QUATERNION_TYPE
+// Quaternion type
+typedef Vector4 Quaternion;
+#define RL_QUATERNION_TYPE
 #endif
 #if !defined(RL_MATRIX_TYPE)
-RL_MATRIX_TYPE
+// Matrix type (OpenGL style 4x4 - right handed, column major)
+typedef struct Matrix {
+float m0, m4, m8, m12;      // Matrix first row (4 components)
+float m1, m5, m9, m13;      // Matrix second row (4 components)
+float m2, m6, m10, m14;     // Matrix third row (4 components)
+float m3, m7, m11, m15;     // Matrix fourth row (4 components)
+} Matrix;
+#define RL_MATRIX_TYPE
 #endif
 #include <math.h>       // Required for: sinf(), cosf(), tan(), atan2f(), sqrtf(), floor(), fminf(), fmaxf(), fabs()
-    return result;
-}
-        else if (length > max)
-        {
-            float scale = max/length;
-            result.x = v.x*scale;
-            result.y = v.y*scale;
-        }
-    }
-    return result;
-}
-    if (fabsf(v.z) < min)
-    {
-        Vector3 tmp = {0.0f, 0.0f, 1.0f};
-        cardinalAxis = tmp;
-    }
-    result.x = v.y*cardinalAxis.z - v.z*cardinalAxis.y;
-    result.y = v.z*cardinalAxis.x - v.x*cardinalAxis.z;
-    result.z = v.x*cardinalAxis.y - v.y*cardinalAxis.x;
-    return result;
-}
-    return result;
-}
-        else if (length > max)
-        {
-            float scale = max/length;
-            result.x = v.x*scale;
-            result.y = v.y*scale;
-            result.z = v.z*scale;
-        }
-    }
-    return result;
-}
-    return result;
-}
-    float sinres = sinf(angle);
-    float cosres = cosf(angle);
-    float t = 1.0f - cosres;
-    result.m0 = x*x*t + cosres;
-    result.m1 = y*x*t + z*sinres;
-    result.m2 = z*x*t - y*sinres;
-    result.m3 = 0.0f;
-    result.m4 = x*y*t - z*sinres;
-    result.m5 = y*y*t + cosres;
-    result.m6 = z*y*t + x*sinres;
-    result.m7 = 0.0f;
-    result.m8 = x*z*t + y*sinres;
-    result.m9 = y*z*t - x*sinres;
-    result.m10 = z*z*t + cosres;
-    result.m11 = 0.0f;
-    result.m12 = 0.0f;
-    result.m13 = 0.0f;
-    result.m14 = 0.0f;
-    result.m15 = 1.0f;
-    return result;
-}
-    return result;
-}
-    if (fabsf(cosHalfTheta) >= 1.0f) result = q1;
-    else if (cosHalfTheta > 0.95f) result = QuaternionNlerp(q1, q2, amount);
-    else
-    {
-        float halfTheta = acosf(cosHalfTheta);
-        float sinHalfTheta = sqrtf(1.0f - cosHalfTheta*cosHalfTheta);
-        if (fabsf(sinHalfTheta) < EPSILON)
-        {
-            result.x = (q1.x*0.5f + q2.x*0.5f);
-            result.y = (q1.y*0.5f + q2.y*0.5f);
-            result.z = (q1.z*0.5f + q2.z*0.5f);
-            result.w = (q1.w*0.5f + q2.w*0.5f);
-        }
-        else
-        {
-            float ratioA = sinf((1 - amount)*halfTheta)/sinHalfTheta;
-            float ratioB = sinf(amount*halfTheta)/sinHalfTheta;
-            result.x = (q1.x*ratioA + q2.x*ratioB);
-            result.y = (q1.y*ratioA + q2.y*ratioB);
-            result.z = (q1.z*ratioA + q2.z*ratioB);
-            result.w = (q1.w*ratioA + q2.w*ratioB);
-        }
-    }
-    return result;
-}
-    if (fourYSquaredMinus1 > fourBiggestSquaredMinus1)
-    {
-        fourBiggestSquaredMinus1 = fourYSquaredMinus1;
-        biggestIndex = 2;
-    }
-    if (fourZSquaredMinus1 > fourBiggestSquaredMinus1)
-    {
-        fourBiggestSquaredMinus1 = fourZSquaredMinus1;
-        biggestIndex = 3;
-    }
-    float biggestVal = sqrtf(fourBiggestSquaredMinus1 + 1.0f)*0.5f;
-    float mult = 0.25f / biggestVal;
-    switch (biggestIndex)
-    {
-        case 0:
-            result.w = biggestVal;
-            result.x = (mat.m6 - mat.m9)*mult;
-            result.y = (mat.m8 - mat.m2)*mult;
-            result.z = (mat.m1 - mat.m4)*mult;
-            break;
-        case 1:
-            result.x = biggestVal;
-            result.w = (mat.m6 - mat.m9)*mult;
-            result.y = (mat.m1 + mat.m4)*mult;
-            result.z = (mat.m8 + mat.m2)*mult;
-            break;
-        case 2:
-            result.y = biggestVal;
-            result.w = (mat.m8 - mat.m2)*mult;
-            result.x = (mat.m1 + mat.m4)*mult;
-            result.z = (mat.m6 + mat.m9)*mult;
-            break;
-        case 3:
-            result.z = biggestVal;
-            result.w = (mat.m1 - mat.m4)*mult;
-            result.x = (mat.m8 + mat.m2)*mult;
-            result.y = (mat.m6 + mat.m9)*mult;
-            break;
-    }
-    return result;
-}
-    return result;
-}
-    Vector3 resAxis = { 0.0f, 0.0f, 0.0f };
-    float resAngle = 2.0f*acosf(q.w);
-    float den = sqrtf(1.0f - q.w*q.w);
-    if (den > EPSILON)
-    {
-        resAxis.x = q.x/den;
-        resAxis.y = q.y/den;
-        resAxis.z = q.z/den;
-    }
-    else
-    {
-        resAxis.x = 1.0f;
-    }
-    *outAxis = resAxis;
-    *outAngle = resAngle;
-}
+#if !defined(EPSILON)
+#define EPSILON 0.000001f
+#endif
+#if !defined(EPSILON)
+#define EPSILON 0.000001f
+#endif
+#if !defined(EPSILON)
+#define EPSILON 0.000001f
+#endif
+#if !defined(EPSILON)
+#define EPSILON 0.000001f
+#endif
+#if !defined(EPSILON)
+#define EPSILON 0.000001f
+#endif
 #endif  // RAYMATH_H
