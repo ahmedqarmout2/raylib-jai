f2f70e80f92f36120a61785a78f6591b6b72f391
Ahmed Qarmout <ahmedqarmout2@gmail.com>
2024-06-09 23:54:14 -0400

    Added core_2d_camera_split_screen example

--- compile_examples.bat
+++ compile_examples.bat
@@ -3,10 +3,9 @@ setlocal
 
 
 pushd examples
-for %%i in (*.jai) do jai -import_dir .. -x64 %%i || exit /b
 
 pushd core
 for %%i in (*.jai) do jai -import_dir ../.. -x64 %%i || exit /b
-
 popd
+
 popd
