b1e2a7b7a7b6ce35871ebcfd70faf723362bc1d0
Ahmed Qarmout <ahmed.qarmout@mail.utoronto.ca>
2024-07-05 17:35:06 -0400

    Added textures_image_rotate example

--- compile_single_example.bat
+++ compile_single_example.bat
@@ -3,6 +3,6 @@ setlocal
 
 pushd examples
 pushd textures
-jai -import_dir ../.. -x64 textures_image_loading.jai || exit /b
+jai -import_dir ../.. -x64 textures_image_rotate.jai || exit /b
 popd
 popd
