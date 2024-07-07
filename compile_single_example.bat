f7cee035aceae6d58bef8995e25dcaec03b748cc
Ahmed Qarmout <ahmed.qarmout@mail.utoronto.ca>
2024-07-07 16:24:59 -0400

    Added textures_particles_blending example

--- compile_single_example.bat
+++ compile_single_example.bat
@@ -3,6 +3,6 @@ setlocal
 
 pushd examples
 pushd textures
-jai -import_dir ../.. -x64 textures_npatch_drawing.jai || exit /b
+jai -import_dir ../.. -x64 textures_particles_blending.jai || exit /b
 popd
 popd
