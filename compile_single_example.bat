9027a391097115f92c5b4563382081080984b1d2
Ahmed Qarmout <ahmed.qarmout@mail.utoronto.ca>
2024-07-07 13:33:06 -0400

    Added textures_npatch_drawing example

--- compile_single_example.bat
+++ compile_single_example.bat
@@ -3,6 +3,6 @@ setlocal
 
 pushd examples
 pushd textures
-jai -import_dir ../.. -x64 textures_mouse_painting.jai || exit /b
+jai -import_dir ../.. -x64 textures_npatch_drawing.jai || exit /b
 popd
 popd
