dc5309cddc7eb1aa42a2b189d0543bec9c754645
Ahmed Qarmout <ahmed.qarmout@mail.utoronto.ca>
2024-08-10 10:43:26 -0400

    Added text to compile all examples script

--- compile_all_examples.bat
+++ compile_all_examples.bat
@@ -11,6 +11,10 @@ pushd core
 for %%i in (*.jai) do jai -import_dir ../.. -x64 -plug Icon -icon ../raylib.ico %%i || exit /b
 popd
 
+pushd text
+for %%i in (*.jai) do jai -import_dir ../.. -x64 -plug Icon -icon ../raylib.ico %%i || exit /b
+popd
+
 pushd textures
 for %%i in (*.jai) do jai -import_dir ../.. -x64 -plug Icon -icon ../raylib.ico %%i || exit /b
 popd
--- compile_single_example.bat
+++ compile_single_example.bat
@@ -3,6 +3,6 @@ setlocal
 
 pushd examples
 pushd text
-jai -import_dir ../.. -x64  -plug Icon -icon ../raylib.ico text_codepoints_loading.jai || exit /b
+jai -import_dir ../.. -x64 -plug Icon -icon ../raylib.ico text_codepoints_loading.jai || exit /b
 popd
 popd
