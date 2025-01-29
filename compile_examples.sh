d8668ac2fa010a22478d2a9a3ae757ec2daf4ce3
Ahmed Qarmout <ahmedqarmout2@gmail.com>
2025-01-29 11:27:10 -0500

    Added compile_examples.sh

---
+++ compile_examples.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd examples
+
+cd audio
+for i in *.jai; do jai-linux -import_dir ../.. -x64 "$i" || break; done
+cd ..
+
+cd core
+for i in *.jai; do jai-linux -import_dir ../.. -x64 "$i" || break; done
+cd ..
+
+cd models
+for i in *.jai; do jai-linux -import_dir ../.. -x64 "$i" || break; done
+cd ..
+
+cd shaders
+for i in *.jai; do jai-linux -import_dir ../.. -x64 "$i" || break; done
+cd ..
+
+cd shapes
+for i in *.jai; do jai-linux -import_dir ../.. -x64 "$i" || break; done
+cd ..
+
+cd text
+for i in *.jai; do jai-linux -import_dir ../.. -x64 "$i" || break; done
+cd ..
+
+cd textures
+for i in *.jai; do jai-linux -import_dir ../.. -x64 "$i" || break; done
+cd ..
+
+cd ..
